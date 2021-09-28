493512efd07a7b2754f23c214b4b1e92508cf08e
Juan Pablo <75744246+JuanPablo1833@users.noreply.github.com>
2021-09-28 10:44:35 -0500

    Add files via upload

---
+++ Comensal.py
@@ -0,0 +1,32 @@
+menu_selec1 = {
+    "menu_selec2":{
+
+#'mesa': 3,
+#'comensal':2,
+#'entrada': "ensalada verde",
+#'medio': "crema de sanaoria",
+#'fuerte': 'filete',
+#'adicionales': 'termino medio, la esnsalda sin ningu tipo de semilla, adereso ranch',
+
+
+
+
+
+},
+'detalle:\n'
+'\tmesa:': 3,
+'\tcomensal:':2,
+'\tentrada:': "ensalada verde",
+'\tmedio:': "crema de sanaoria",
+'\tfuerte:': 'filete',
+'\tadicionales:': 'termino medio, la esnsalda sin ningu tipo de semilla, adereso ranch',
+
+    }
+ 
+llaves = menu_selec1.keys()
+#print(llaves)
+
+for llave in llaves:
+
+    print(llave, menu_selec1[llave])
+    #print(menu_selec1[llave])
